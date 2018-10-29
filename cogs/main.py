3b2f88011c7d17d2e3dc6b36483dab3aee8c48b9
EnterNewName <lifez.memes@gmail.com>
2018-10-29 01:41:24 -0500

    shit shit shit

--- cogs/main.py
+++ cogs/main.py
@@ -1,4 +1,4 @@
-import discord, asyncio, json, time, datetime, psutil
+import discord, asyncio, json, time, datetime, psutil, aiohttp
 from time import ctime
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
