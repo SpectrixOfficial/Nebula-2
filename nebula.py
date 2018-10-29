ba7561a0259afcb07527863abbf6dd5c63dbb117
EnterNewName <lifez.memes@gmail.com>
2018-10-29 03:36:46 -0500

    configuring

--- nebula.py
+++ nebula.py
@@ -51,8 +51,7 @@ class Nebula_Bot(commands.Bot):
         print("Discord.py Version : {}".format(pkg_resources.get_distribution("discord.py").version))
         print(f"{self.user} Is Online")
         print(f"Guild Count : {len(self.guilds)}\n")
-        db = await self.create_pool()
-        bot.db = db
+        self.db = await self.create_pool()
 
     async def on_guild_join(self, guild):
         await self.presencehandler()         
